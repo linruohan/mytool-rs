6c3a6fd9ad95ec746304531af687120f784b73ca
linruohan <mjt1220@126.com>
2024-11-16 11:59:37 +0800

    add icon

--- build.rs
+++ build.rs
@@ -1,8 +1,23 @@
-fn main() {
-    // actions
+fn main() -> anyhow::Result<()> {
+    compile_gresources()?;
+    #[cfg(windows)]
+    compile_icon_winres()?;
+
+    Ok(())
+}
+fn compile_icon_winres() -> anyhow::Result<()> {
+    use anyhow::Context;
+    let mut res = winresource::WindowsResource::new();
+    res.set("OriginalFileName", "mytool-rs.exe");
+    res.set_icon("./data/icons/mytool.ico");
+    res.compile()
+        .context("Failed to compile winresource resource")
+}
+fn compile_gresources() -> anyhow::Result<()> {
     glib_build_tools::compile_resources(
         &["data"],
         "data/resources.gresource.xml",
         "mytool.gresource",
     );
+    Ok(())
 }
