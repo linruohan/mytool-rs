7041088b401fe1394e7995b1127a2154c2d4102a
linruohan <mjt1220@126.com>
2024-11-30 12:30:11 +0800

    adapt gschema files

--- build-aux/build.sh
+++ build-aux/build.sh
@@ -1,7 +1,8 @@
 #/usr/bin/env bash
-if uname -s | grep MINGW; then
+if uname -s | grep MSYS; then
     # window
     glib_schemas_dir="/c/ProgramData/glib-2.0/schemas/"
+    glib_schemas_dir="../target/share/glib-2.0/schemas/"
 else
     # linux
     glib_schemas_dir="$HOME/.local/share/glib-2.0/schemas"
